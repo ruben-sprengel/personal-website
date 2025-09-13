355b8562efa407515178f99de69d69e85182ce77
Ruben Sprengel <132717192+ruben-sprengel@users.noreply.github.com>
2025-09-14 00:17:46 +0200

    #10 feat: optimize seo, bump version (#11)
    * #10 feat: optimize seo, bump version
    
    * #10 fix: formatting

--- website/src/routes/impressum/+page.svelte
+++ website/src/routes/impressum/+page.svelte
@@ -2,6 +2,11 @@
 	import { env } from '$env/dynamic/public';
 </script>
 
+<svelte:head>
+	<title>Ruben Sprengel | Impressum</title>
+	<meta name="robots" content="noindex" />
+</svelte:head>
+
 <section class="mx-auto min-h-screen w-full max-w-4xl px-4 py-16">
 	<h1 class="mb-8 text-4xl font-bold">Impressum</h1>
 
